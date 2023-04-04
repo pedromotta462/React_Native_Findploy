e20f695c81bc42c983bd7be6e131ee5869087076
pedromotta462 <pedromotta1d2015@gmail.com>
2023-04-04 17:37:32 -0300

    First commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+
+const Welcome = () => {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.userName}>Welcome, Pedro Motta!</Text>
+      <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+    </View>
+  )
+}
+
+export default Welcome
